3157126bca428e232c0a391a3ed1ea80cf1dc47c
NansyT <nanna.tronbjerg@gmail.com>
2020-11-20 09:03:50 +0100

    d

--- MyPain/MyPain/MainWindow.xaml.cs
+++ MyPain/MyPain/MainWindow.xaml.cs
@@ -86,7 +86,6 @@ namespace MyPain
         {
             paintArea.DefaultDrawingAttributes.StylusTip = StylusTip.Rectangle;
             paintArea.SnapsToDevicePixels = true;
-            
         }
     }
 }
